ec30504c7bce594cbfc262c0123423243b94724f
priyansh0.52007 <129646454+priyansh052007@users.noreply.github.com>
2026-04-19 17:20:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const all_tabs = document.querySelectorAll('.tab');
 
+all_tabs[0].classList.add('active');
+
 all_tabs.forEach(find_tab => {
   find_tab.addEventListener('click', () => {
     all_tabs.forEach(find_tab => find_tab.classList.remove('active'));
