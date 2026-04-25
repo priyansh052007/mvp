f465b764550812146a4413908644d09bfd52e791
priyansh0.52007 <129646454+priyansh052007@users.noreply.github.com>
2026-04-25 13:46:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ main_tabs.forEach(click_tab => {
 });
 
 
+const tabs = document.querySelectorAll('.tab');
+
+tabs[0].classList.add('active');
+
+tabs.forEach(select_tab => {
+  select_tab.addEventListener('click', () => {
+    tabs.forEach(select_tab => select_tab.classList.remove('active'));
+    select_tab.classList.add('active');
+  });
+});
+
+
 const todo_tabs = document.querySelectorAll('.todo_tab');
 
 todo_tabs[0].classList.add('active');
@@ -20,4 +32,5 @@ todo_tabs.forEach(find_tab => {
     find_tab.classList.add('active');
   });
 });
+
 
