9ed8ab3720f4e2fbde5b16ff6c26a4da182cac23
priyansh0.52007 <129646454+priyansh052007@users.noreply.github.com>
2026-05-15 23:04:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ function openMainProfile() {
   profile.classList.remove("active");
   app.style.marginTop = "0";
   main_profile.classList.add("active");
-  app.style.marginRight = "100%";
+  app.style.marginLeft = "100vw";
 }
 
 function closeMainProfile() {
   main_profile.classList.remove("active");
-  app.style.marginRight = "0";
+  app.style.marginLeft = "0";
 }
 
 
