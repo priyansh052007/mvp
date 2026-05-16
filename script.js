66cc3e3baf05bd800b8468b3adf68b351a7251f6
priyansh0.52007 <129646454+priyansh052007@users.noreply.github.com>
2026-05-16 10:17:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,18 @@ const profile = document.getElementById("profile");
 const main_profile = document.getElementById("main_profile");
 
 function openProfile() {
-  /*
   profile.classList.add("active");
   app.style.marginTop = "45dvh";
-  */
-  app.style.Top = "45dvh";
 }
 
 function closeProfile() {
-  /*
   profile.classList.remove("active");
   app.style.marginTop = "0";
-  */
 }
 
 function openMainProfile() {
-  /*
   profile.classList.remove("active");
   app.style.marginTop = "0";
-  */
   main_profile.classList.add("active");
 }
 
