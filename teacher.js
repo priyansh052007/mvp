f26bc0b8e68484994549e7057336c1775449533b
priyansh0.52007 <129646454+priyansh052007@users.noreply.github.com>
2026-05-26 15:42:17 +0530

    Update teacher.js

--- teacher.js
+++ teacher.js
@@ -106,3 +106,168 @@ function setTab(id, value) {
 
 function updateTabs() {
   let h1, t1, t2, t3, t4, t5;
+
+  if (currentTab === "school" && currentOption === "a") {
+    h1 = "School";
+    t1 = "School";
+    t2 = "Event";
+    t3 = "Calendar";
+    t4 = "Attendance";
+    t5 = "Notification";
+    c1 = "School";
+  }
+  else if (currentTab === "school" && currentOption === "b") {
+    h1 = "School";
+    t1 = "School";
+    t2 = "Event";
+    t3 = "Calendar";
+    t4 = "Attendance";
+    t5 = "Notification";
+    c1 = "Event";
+  }
+  else if (currentTab === "school" && currentOption === "c") {
+    h1 = "School";
+    t1 = "School";
+    t2 = "Event";
+    t3 = "Calendar";
+    t4 = "Attendance";
+    t5 = "Notification";
+    c1 = "Calendar";
+  }
+  else if (currentTab === "school" && currentOption === "d") {
+    h1 = "School";
+    t1 = "School";
+    t2 = "Event";
+    t3 = "Calendar";
+    t4 = "Attendance";
+    t5 = "Notification";
+    c1 = "Attendance";
+  }
+  else if (currentTab === "school" && currentOption === "e") {
+    h1 = "School";
+    t1 = "School";
+    t2 = "Event";
+    t3 = "Calendar";
+    t4 = "Attendance";
+    t5 = "Notification";
+    c1 = "Notification";
+  }
+
+  else if (currentTab === "class" && currentOption === "a") {
+    h1 = "Class";
+    t1 = "Class";
+    t2 = "Class Attendance";
+    t3 = "";
+    t4 = "";
+    t5 = "";
+    c1 = "Class";
+  }
+  else if (currentTab === "class" && currentOption === "b") {
+    h1 = "Class";
+    t1 = "Class";
+    t2 = "Class Attendance";
+    t3 = "";
+    t4 = "";
+    t5 = "";
+    c1 = "Class Attendance";
+  }
+
+  else if (currentTab === "subject" && currentOption === "a") {
+    h1 = "Subject";
+    t1 = "Subject";
+    t2 = "Missed Topic Student";
+    t3 = "Subject Activity";
+    t4 = "";
+    t5 = "";
+    c1 = "Subject";
+  }
+  else if (currentTab === "subject" && currentOption === "b") {
+    h1 = "Subject";
+    t1 = "Subject";
+    t2 = "Missed Topic Student";
+    t3 = "Subject Activity";
+    t4 = "";
+    t5 = "";
+    c1 = "Missed Topic Student";
+  }
+  else if (currentTab === "subject" && currentOption === "c") {
+    h1 = "Subject";
+    t1 = "Subject";
+    t2 = "Missed Topic Student";
+    t3 = "Subject Activity";
+    t4 = "";
+    t5 = "";
+    c1 = "Subject Activity";
+  }
+
+  else if (currentTab === "student" && currentOption === "a") {
+    h1 = "Student";
+    t1 = "Student";
+    t2 = "Student Missed Topic";
+    t3 = "";
+    t4 = "";
+    t5 = "";
+    c1 = "Student";
+  } 
+  else if (currentTab === "student" && currentOption === "b") {
+    h1 = "Student";
+    t1 = "Student";
+    t2 = "Student Missed Topic";
+    t3 = "";
+    t4 = "";
+    t5 = "";
+    c1 = "Student Missed Topic";
+  }
+
+  else if (currentTab === "timetable" && currentOption === "a") {
+    h1 = "Timetable";
+    t1 = "Timetable";
+    t2 = "Date Specific Timetable";
+    t3 = "Exam Timetable";
+    t4 = "";
+    t5 = "";
+    c1 = "Timetable";
+  } 
+  else if (currentTab === "timetable" && currentOption === "b") {
+    h1 = "Timetable";
+    t1 = "Timetable";
+    t2 = "Date Specific Timetable";
+    t3 = "Exam Timetable";
+    t4 = "";
+    t5 = "";
+    c1 = "Date Specific Timetable";
+  }
+  else if (currentTab === "timetable" && currentOption === "c") {
+    h1 = "Timetable";
+    t1 = "Timetable";
+    t2 = "Date Specific Timetable";
+    t3 = "Exam Timetable";
+    t4 = "";
+    t5 = "";
+    c1 = "Exam Timetable";
+  }
+
+  document.getElementById("header_1").innerText = h1;
+  /*
+  document.getElementById("tab_1").innerText = t1;
+  document.getElementById("tab_2").innerText = t2;
+  document.getElementById("tab_3").innerText = t3;
+  document.getElementById("tab_4").innerText = t4;
+  document.getElementById("tab_5").innerText = t5;
+  */
+  setTab("tab_1", t1);
+  setTab("tab_2", t2);
+  setTab("tab_3", t3);
+  setTab("tab_4", t4);
+  setTab("tab_5", t5);
+}
+
+
+
+
+
+
+
+
+
+
